6707744bbe1810e6edf166a515252348f5291b2d
resurtm <resurtm@gmail.com>
2025-01-04 14:37:49 +0100

    Day 01, part 1, cleanup, add tests, preparations

--- src/day01.zig
+++ src/day01.zig
@@ -1,24 +1,81 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
+const expect = std.testing.expect;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    // const rawInput: []const u8 = "R2, L3";
-    const rawInput: []const u8 = "R5, L5, R5, R3";
-
-    const movements = try parseInput(rawInput, allocator);
-    defer movements.deinit();
-
-    for (movements.items, 0..) |it, idx| {
-        std.debug.print("{} - {} - {}\n", .{ idx, it.side, it.steps });
-    }
+    _ = try solveInternal("R5, L5, R5, R3", allocator);
 }
 
-fn parseInput(rawInput: []const u8, allocator: std.mem.Allocator) !ArrayList(Movement) {
+fn solveInternal(input: []const u8, allocator: std.mem.Allocator) !u32 {
+    const movements = try parseInput(input, allocator);
+    defer movements.deinit();
+
+    var x: i32 = 0;
+    var y: i32 = 0;
+    var dir: Direction = .north;
+
+    for (movements.items) |it| {
+        switch (it.side) {
+            .left => {
+                switch (dir) {
+                    .north => {
+                        dir = .west;
+                    },
+                    .south => {
+                        dir = .east;
+                    },
+                    .east => {
+                        dir = .north;
+                    },
+                    .west => {
+                        dir = .south;
+                    },
+                }
+            },
+            .right => {
+                switch (dir) {
+                    .north => {
+                        dir = .east;
+                    },
+                    .south => {
+                        dir = .west;
+                    },
+                    .east => {
+                        dir = .south;
+                    },
+                    .west => {
+                        dir = .north;
+                    },
+                }
+            },
+        }
+
+        switch (dir) {
+            .north => {
+                y -= it.steps;
+            },
+            .south => {
+                y += it.steps;
+            },
+            .east => {
+                x += it.steps;
+            },
+            .west => {
+                x -= it.steps;
+            },
+        }
+    }
+
+    return @abs(x) + @abs(y);
+}
+
+fn parseInput(input: []const u8, allocator: std.mem.Allocator) !ArrayList(Movement) {
     var movements = ArrayList(Movement).init(allocator);
-    var it = std.mem.splitSequence(u8, rawInput, ", ");
+    var it = std.mem.splitSequence(u8, input, ", ");
     while (it.next()) |x| {
         var movement = Movement{
             .side = Side.left,
@@ -42,3 +99,30 @@ fn parseInput(rawInput: []const u8, allocator: std.mem.Allocator) !ArrayList(Mov
 const Movement = struct { side: Side, steps: i32 };
 const Side = enum { left, right };
 const Direction = enum { north, south, east, west };
+
+test "case 1" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const actual = try solveInternal("R2, L3", allocator);
+    try expect(actual == 5);
+}
+
+test "case 2" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const actual = try solveInternal("R2, R2, R2", allocator);
+    try expect(actual == 2);
+}
+
+test "case 3" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const actual = try solveInternal("R5, L5, R5, R3", allocator);
+    try expect(actual == 12);
+}
